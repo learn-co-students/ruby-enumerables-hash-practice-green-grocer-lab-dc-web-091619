ae61e32bbc30c2d9df92ee920b6aad94e775c66c
collaborative-bug-2010 <detrodoniv@yahoo.com>
2019-09-12 15:27:49 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -17,12 +17,12 @@ def consolidate_cart(cart)
 
 def apply_coupons(cart, coupons)
  coupons.each do |coupon|
-   item = coupon[item][:count]
+   item = coupon[:item]
    if cart.has_key?[item] 
      if cart[item][:count] >= coupon[:num] && !cart.has_key?["#{item}W/COUPON"]
        cart["#{item}W/COUPON"] = {price: coupon[:cost] / coupon[:num] , clearance: cart[item][:clearance] , count: coupon[num]}
         cart[item][:count] -= coupon[:num]
-   elseif cart[:item][:count] >= coupon[:num] && !cart.has_key?["#{item}W/COUPON"]
+   elseif cart[item][:count] >= coupon[:num] && !cart.has_key?["#{item}W/COUPON"]
     cart["#{item}W/COUPON"][:count] += counpon[num]
 end
 cart[item][:count] -= coupon[:num]
